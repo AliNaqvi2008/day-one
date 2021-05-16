da0ede9913ec6ca264973999d36658c9488d4c4b
AliNaqvi2008 <84286239+AliNaqvi2008@users.noreply.github.com>
2021-05-16 19:10:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+    //This command is used to print on
+    //Google
+    console.log('‍Welcome to Javascript');
+    //Variable    
+    
+    //let name = 'Ali';
+    //console.log(name);
+
+    //let age = 12;
+    //console.log(age)
+
+    let firstname = 'Ali'
+    let lastname = 'Naqvi'
+    let space = ' '; // for creating space
+    
+    console.log(firstname + space + lastname) //for professionals
+    //12
+    console.log(firstname +' '+ lastname +' '+ 'my age is' +' '+ age) //for beginners
+    let age = prompt("What is your age?")
+    
+    document.getElementById('myage').innerHTML=age;
+
+    //graphics designing, web development and javascript
+    console.log(firstname +' '+ lastname +' '+ 'my hobbies are' +' '+ hobbies) //for beginners
+    let hobbies = prompt("What are your hobbies?")
+    
+    document.getElementById('myhobbies').innerHTML=age;
